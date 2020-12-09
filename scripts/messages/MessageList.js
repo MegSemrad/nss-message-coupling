d4c379d95f9b8e615f466ec03ebe867e8e98d1e4
MegSemrad <mlsemrad753@gmail.com>
2020-12-08 20:16:51 -0600

    added study notes

--- scripts/messages/MessageList.js
+++ scripts/messages/MessageList.js
@@ -9,33 +9,39 @@ const eventHub = document.querySelector(".container")
 */
 
 
-// document.querySelector(".themes").addEventListener("click", e => {
-    eventHub.addEventListener("colorChosen", event => {
-        const color = event.detail.color
+eventHub.addEventListener("colorChosen", event => {
+    const color = event.detail.color
         
-        /* This below is saying take whatever color is taken from the 
-        end of the id (ex.btnTheme--purple/green/etc) when that specific
-        button  is clicked. THen use that word and turn it into a class 
-        on the contentTarget, which in this case is the element with
-        ".messages"*/
-        /* the empty array before that means switch the class back to empty. Therefore 
-        if one button was clicked after the other there wouldn't be two classes with 
-        two colors on it but rather the most current one */
-        contentTarget.classList = []
-        contentTarget.classList.add(color)
-    })
+    /* This below is saying take whatever color is taken from the 
+    end of the id (ex.btnTheme--purple/green/etc) when that specific
+    button  is clicked. Then use that word and turn it into a class 
+    on the contentTarget, which in this case is the element with
+    ".messages"*/
+    /* the empty array before that means switch the class back to empty. Therefore 
+    if one button was clicked after the other there wouldn't be two classes with 
+    two colors on it but rather the most current one */
+    contentTarget.classList = []
+    contentTarget.classList.add(color)
+})
     
     /*
     - e stands for event it has a property of target to select that and get its id 
     */
 
 
-
+/*
+    MessageList(), like ThemeButtons(), has to be imported to main.js
+    so it will generate as soon as the browser is pull up
+*/
 export const MessageList = () => {
     const allMessages = useMessages()
     render(allMessages)
 }
 
+/*
+ The .map() method creates a new array with the results of calling 
+ a function for every array element.
+*/
 
 const render = messageArray => {
     const convertedMessages = messageArray.map(messageObject => {
@@ -45,5 +51,3 @@ const render = messageArray => {
     const combinedSections = convertedMessages.join("")
     contentTarget.innerHTML = combinedSections
 }
-
-
